102ddce41d2bf798583b46986d9017fc4bc8a219
tvo93 <tvo93@uw.edu>
2018-03-15 02:32:51 -0700

    final

--- app/src/main/java/interview/handimaps/thuanvoapp/FeedbackFragment.java
+++ app/src/main/java/interview/handimaps/thuanvoapp/FeedbackFragment.java
@@ -9,7 +9,6 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
